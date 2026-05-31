8e20f1858bbcb98741a93c02916d2ff943b63714
Andreina Di Brigida <Akiarakirax3@gmail.com>
2026-05-31 17:44:12 -0400

    Update for vercel

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,6 @@
 import type { NextConfig } from "next";
 
-const isGithubPages = process.env.GITHUB_PAGES === "true";
-const basePath = isGithubPages ? "/morales-solutions" : undefined;
-
 const nextConfig: NextConfig = {
-  output: "export",
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
-  basePath,
-  assetPrefix: basePath,
 };
 
 export default nextConfig;
